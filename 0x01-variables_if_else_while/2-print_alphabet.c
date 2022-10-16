af6400a91a667420e433504dd8dd6f2e9464195a
tiptopy <tiptopywafula@gmail.com>
2022-10-16 16:57:24 -0700

    my wonderful alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+  *main - The main entry point for my program
+  *argument: This program taked no arguments
+  *Return: this program returns zero by default
+*/
+
+int main()
+{
+	for (char c='a' ; c <= 'z' ; c++)
+		putchar(c);
+}
