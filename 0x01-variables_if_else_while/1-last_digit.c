0007da800cafa304ce072ad0c205c5c3d9f07b0a
tiptopy <tiptopywafula@gmail.com>
2022-10-16 16:47:45 -0700

    updated berry errors

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -6,9 +6,9 @@
 
 
 /**
-  *main: this is the main entry point for our program
+  *main - this is the main entry point for our program
   *argument: thid program taked no argument
-  *return: this program returns a zero by defaulr
+  *Return: this program returns a zero by defaulr
 */
 int main(void)
 
@@ -23,7 +23,7 @@ int main(void)
 		printf("and is greater than 5\n");
 	else if (n % 10 == 0)
 		printf("and is 0\n");
-	else if (n % 10 <6 && n%10 != 0)
+	else if (n % 10 < 6 && n % 10 != 0)
 		printf("and is less than 6 and not 0\n");
 	return (0);
 
