cbcb955ee71f238251bf38d4ea5fe526d9341b2e
tiptopy <tiptopywafula@gmail.com>
2022-10-16 18:37:28 -0700

    print hexa decimaals

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+  *main - This is the main entry point for my program.
+  *arguments: This functions passes no aeguments.
+  *Return: This program returns a zero by default.
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 16; i++)
+		if (i>9)
+		{	int k;
+			for (k = 0; k < 6; k++)
+				putchar(k + 'a');
+		break;
+		}
+		else
+			putchar(i + '0');
+	putchar('\n');
+	return (0);
+}
