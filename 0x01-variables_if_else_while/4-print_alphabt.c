ad324bbd6911b213d0634ca901d2935136f8ddf6
tiptopy <tiptopywafula@gmail.com>
2022-10-16 17:27:26 -0700

    alphabet with if

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+  *main - this is the main entry point for my program
+  *argument: this program takes no argument
+  *Return: This program returns a zero by default
+*/
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c == 'q' || c == 'e')
+			continue;
+		else
+			putchar(c);
+	}
+	return (0);
+}
