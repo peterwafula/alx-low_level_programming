e3b2d485e603cc0d783b01dddcb93eed5727dd73
tiptopy <tiptopywafula@gmail.com>
2022-10-16 17:34:26 -0700

    Print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+  *main - This is the main entry point for my program"
+  *argument: This program takes no argument
+  *Return: This program returns a zero by default
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
