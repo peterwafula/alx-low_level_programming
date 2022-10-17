1b3120bfd40a512256a8a5948cbb2cc44825d0cd
tiptopy <tiptopywafula@gmail.com>
2022-10-16 18:48:13 -0700

    Combined

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+  *main - This is the entry point for my program
+  *argument: This function takes no argument
+  *Return: This funvtion returns a default zero.
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '8');
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
