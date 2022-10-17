84f858f2799a2399914fb8ec81008c42151aa5cc
tiptopy <tiptopywafula@gmail.com>
2022-10-16 17:16:01 -0700

    all alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+  *main - This is the main entry point for my program
+  *argument: My program takes no argument
+  *Return: This program returns a zero by default
+*/
+
+int main(void)
+{
+	char c,C;
+	for (c = 'a';c <= 'z';c++)
+		putchar(c);
+	for (C='A';C<='Z';C++)
+		putchar(C);
+	putchar('\n')
+	return (0);
+}
+
