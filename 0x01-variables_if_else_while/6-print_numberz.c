ab117bac81a664607a59a6cdc0d51c80ef8e7a9b
tiptopy <tiptopywafula@gmail.com>
2022-10-16 17:40:37 -0700

    print int as char

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+  *main - This is the main entry point of my program.
+  *arguments: This program taked no argument
+  *Return: This program returns zero by default
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
