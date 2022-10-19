70b3e1c1e4e07ca7bdedd546e357097c8df26c01
tiptopy <tiptopywafula@gmail.com>
2022-10-19 15:59:44 -0700

    Multiplication without much limits

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+#include <string.h>
+
+#include <stdio.h>
+
+/**
+  *print_times_table -Multiplication table
+  *@n: This function takes n as table limit
+  *Return: This function returns nothing
+*/
+
+void print_times_table(int n)
+{
+	int i, j;
+	
+	if (n > 15 || n < 0)
+		break;
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			int prd = i * j;
+
+			if (j != 0 && prd < 10)
+				printf(" ");
+			if (j == 9)
+				printf("%d\n", i * j);
+			else
+				printf("%d, ", i * j);
+		}
+	}
+}
