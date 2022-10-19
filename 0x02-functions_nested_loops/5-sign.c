ad998e6311ad8cca5ca5a52d26244acbe3800e10
tiptopy <tiptopywafula@gmail.com>
2022-10-19 13:24:23 -0700

    sign notations

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+  *print_sign - prints sign of an interger
+  *@n: The interger to check sign
+  *Return: Returns an interger.
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n = 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
