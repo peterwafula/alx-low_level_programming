8566987a87b6b6401927c831f76ac6ee73a20832
tiptopy <tiptopywafula@gmail.com>
2022-10-19 15:31:28 -0700

    add to a point

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,12 +10,14 @@
 
 void print_to_98(int n)
 {
-	for (n; n <= 98; n++)
+	int i;
+
+	for (i = n; i <= 98; i++)
 	{
-		if (n ==98)
-			printf("%d", n);
+		if (i ==98)
+			printf("%d", i);
 		else
-			printf("%d, ", n);
+			printf("%d, ", i);
 	}
 	printf("\n");
 }
