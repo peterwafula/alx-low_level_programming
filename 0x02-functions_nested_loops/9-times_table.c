9d732f07fe5f80e0c2608d076beeba119a812886
tiptopy <tiptopywafula@gmail.com>
2022-10-19 14:43:03 -0700

    Multiplication table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+#include <stdio.h>
+
+/**
+  *times_table -Multiplication table
+  *Arguments: This function takes no argument
+  *Return: This function returns nothing
+*/
+
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (j == 9)
+				printf("%d\n", 1 * j);
+			else
+				printf("%d, ", i * j);
+		}
+	}
+}
