598b096f0b613ade11f553f1d37bfca37b0ec706
tiptopy <tiptopywafula@gmail.com>
2022-10-19 15:43:12 -0700

    updated betty bug

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -16,7 +16,7 @@ void print_to_98(int n)
 	{
 		for (i = n; i <= 98; i++)
 		{
-			if (i ==98)
+			if (i == 98)
 			{
 				printf("%d", i);
 				break;
@@ -26,14 +26,14 @@ void print_to_98(int n)
 	}
 	else
 	{
-		for (i = n; i >=98; i--)
+		for (i = n; i >= 98; i--)
 		{
 			if (i == 98)
 			{
-				printf("%d",i);
+				printf("%d", i);
 				break;
 			}
-			printf("%d, ",i);
+			printf("%d, ", i);
 		}
 	}
 	printf("\n");
