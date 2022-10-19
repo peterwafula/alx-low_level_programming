8e1003fb70500b4f1d2bfbc8a17ed173c905edd6
tiptopy <tiptopywafula@gmail.com>
2022-10-19 15:28:10 -0700

    print to a point

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+#include <stdio.h>
+
+/**
+  *print_to_98 - This function prints from a point to 98
+  *@n: This is the beginning point
+  *Return: This function doesn't return anything
+*/
+
+void print_to_98(int n);
+{
+	for (n; n <= 98; n++)
+	{
+		if (n ==98)
+			printf("%d", n);
+		else
+			printf("%d, ", n);
+	}
+	printf("\n");
+}
