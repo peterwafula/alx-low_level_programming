5b1a9f7ecd8cc3ecc30fa0056b98a4d7590b7a71
tiptopy <tiptopywafula@gmail.com>
2022-10-19 16:24:54 -0700

    add upto 1024

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+#include <stdio.h>
+
+/**
+  *main - Entry to progarm
+  *argument: Takes no argument
+  *Return: Returns void.
+*/
+
+void main(void)
+{
+	int i, sum;
+
+	sum = 0;
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			sum = sum + i;;
+	}
+	printf("%d", sum);
+}
