66cd050207d2303f0a148e6665bf9503fa18c7bb
tiptopy <tiptopywafula@gmail.com>
2022-10-19 15:41:19 -0700

    add upto 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,14 +12,29 @@ void print_to_98(int n)
 {
 	int i;
 
-	for (i = n; i <= 98; i++)
+	if (n <= 98)
 	{
-		if (i ==98)
+		for (i = n; i <= 98; i++)
 		{
-			printf("%d", i);
-			break;
+			if (i ==98)
+			{
+				printf("%d", i);
+				break;
+			}
+			printf("%d, ", i);
 		}
-		printf("%d, ", i);
+	}
+	else
+	{
+		for (i = n; i >=98; i--)
+		{
+			if (i == 98)
+			{
+				printf("%d",i);
+				break;
+			}
+			printf("%d, ",i);
+		}
 	}
 	printf("\n");
 }
