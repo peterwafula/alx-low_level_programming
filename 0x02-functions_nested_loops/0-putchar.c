512c8a847ff7da0fdc9355fd061f10741f7ef9fc
tiptopy <tiptopywafula@gmail.com>
2022-10-19 02:21:27 -0700

    Putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+  *main - This is the main entry of the program
+  *arguments: This program takes no argument
+  *Return: This program returns nothing
+*/
+
+void main()
+{
+	printf("_putchar\n");
+}
