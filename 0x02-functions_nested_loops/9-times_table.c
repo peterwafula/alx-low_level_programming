01b834d05ec6935e84f2f14c82fdf8b39d3e3667
tiptopy <tiptopywafula@gmail.com>
2022-10-19 14:58:44 -0700

    multiplyL

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,14 +12,16 @@
 
 void times_table(void)
 {
-	int i, j;
+	int i, j, ans;
+
+	ans = i * j;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			if (i * j <= 9)
-				_putchar(' ');
+			if (ans <= 9)
+				printf(" ");
 			if (j == 9)
 				printf("%d\n", i * j);
 			else
