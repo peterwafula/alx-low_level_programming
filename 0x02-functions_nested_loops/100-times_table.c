c538428de43d97e70459be9b709a3c7d5c7f2b46
tiptopy <tiptopywafula@gmail.com>
2022-10-19 16:02:02 -0700

    update multiply code

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -14,12 +14,12 @@ void print_times_table(int n)
 {
 	int i, j;
 	
-	if (n > 15 || n < 0)
-		break;
 	for (i = 0; i <= n; i++)
 	{
 		for (j = 0; j <= n; j++)
 		{
+			if (n > 15 || n < 0)
+				break;
 			int prd = i * j;
 
 			if (j != 0 && prd < 10)
