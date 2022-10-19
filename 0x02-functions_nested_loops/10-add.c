7b50ff2558a6bab276684a0670f5d8f89f142dbc
tiptopy <tiptopywafula@gmail.com>
2022-10-19 15:21:05 -0700

    add two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  *add - This function adds two intergers
+  *@a: This is the first interger
+  *@b: This is the second interger
+  *Return: This function returns the sum of the two.
+*/
+
+int add(int a, int b)
+{
+	return (a + b);
+}
