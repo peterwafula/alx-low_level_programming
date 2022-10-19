a12d5459ef022858aafa69c36884bd753bfae274
tiptopy <tiptopywafula@gmail.com>
2022-10-19 15:33:54 -0700

    add to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,9 +15,11 @@ void print_to_98(int n)
 	for (i = n; i <= 98; i++)
 	{
 		if (i ==98)
+		{
 			printf("%d", i);
-		else
-			printf("%d, ", i);
+			break;
+		}
+		printf("%d, ", i);
 	}
 	printf("\n");
 }
