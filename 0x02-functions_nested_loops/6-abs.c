837f851b4825a860bc980b2c79a0beacbb391ee2
tiptopy <tiptopywafula@gmail.com>
2022-10-19 13:33:33 -0700

    Absolute values

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+#include <stdlib.h>
+
+/**
+  *_abs - Checks an absolute value
+  *@a: The number to be returned its absolute
+  *Return: the absolute value
+*/
+
+int _abs(int a)
+{
+	return(abs(a));
+}
