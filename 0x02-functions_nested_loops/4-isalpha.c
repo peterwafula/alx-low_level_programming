431ca839100cfa7378473afda0c0f9dec03b310c
tiptopy <tiptopywafula@gmail.com>
2022-10-19 09:09:06 -0700

    is alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+#include <ctype.h>
+
+/**
+  *_isalpha - Checks whether argument passed is alpha
+  *@c: The argument to be checked
+  *Return: Returns either 1 or 0
+*/
+
+int _isalpha(int c);
+{
+	if (isalpha(c))
+		return (1);
+	else
+		return (0);
+}
