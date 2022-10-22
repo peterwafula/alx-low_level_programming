df7fb724dc89e147832b207df93bd8b8e5f10172
tiptopy <tiptopywafula@gmail.com>
2022-10-22 03:11:31 -0700

    done

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -17,10 +17,10 @@ void print_remaining_days(int month, int day, int year)
 	{
 		if (month > 2)
 		{
-			day--;
+			day++;
 		}
 	printf("Day of the year: %d\n", day;
-	printf("Remaining days: %d\n", 365 - day);
+	printf("Remaining days: %d\n", 366 - day);
 	}
 	else
 	{
