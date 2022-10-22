219c5eec3c902b882361c2187a53ebbf583cf96b
tiptopy <tiptopywafula@gmail.com>
2022-10-22 00:36:18 -0700

    my first test case

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  *main -This functions test for positive or negative occurence
+  *Return: 0
+*/
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+
+	positive_or_negative(i);
+	return (0);
+}
