df2ab939075433ee12f23aa2cc1c3ba0808bb7c5
tiptopy <tiptopywafula@gmail.com>
2022-10-22 22:44:38 -0700

    Print numbers multiple times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+  *more_numbers - Prints numbers 14 times
+  *
+  *Return: void
+*/
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			_putchar(j + '0');
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
