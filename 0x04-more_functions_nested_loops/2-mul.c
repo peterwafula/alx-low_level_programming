50fd90e7dfe5f070c9ac0b0b98e36ceca5cb7644
tiptopy <tiptopywafula@gmail.com>
2022-10-22 22:21:17 -0700

    Function to find product of two intergers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  *mul - Returns product of two intergers
+  *@a: Filrst integer
+  *@b: Second interger
+  *Return: The product of the two
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
