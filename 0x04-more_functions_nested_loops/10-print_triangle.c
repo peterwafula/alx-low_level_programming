a945daaad8fc75919a1b14c3e1294bc2b2944be4
tiptopy <tiptopywafula@gmail.com>
2022-10-24 16:00:04 -0700

    update triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,17 +10,14 @@ void print_triangle(int size)
 {
 	int i, j;
 
-	if (size <= 0)
-		_putchar('\n');
-	else
+	if (size > 0)
 	{
 		for (i = 1; i <= size; i++)
 		{
 			for (j = size - i; j > 0; j--)
-			_putchar(' ');
-			for (j = 0; j < h; j ++)
+				_putchar(' ');
+			for (j = 0; j < h; j++)
 				_putchar('#');
-			_putchar('\n');
 			if (i == size)
 				continue;
 			_putchar('\n');
