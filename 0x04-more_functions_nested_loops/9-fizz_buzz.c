a84db5e9555a6723ea6dd122db702344175560f7
tiptopy <tiptopywafula@gmail.com>
2022-10-24 15:07:51 -0700

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,15 +14,17 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0)
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
 		{
 			printf("Fizz");
-			continue;
 		}
 		else if (i % 5 == 0)
 		{
 			printf("Buzz");
-			continue;
 		}
 		else
 			printf("%d", i);
