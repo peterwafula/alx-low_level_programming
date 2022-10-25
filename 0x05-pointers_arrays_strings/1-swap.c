f8cf2e94a511acdc47c1b039fff27e8f3d09de82
tiptopy <tiptopywafula@gmail.com>
2022-10-25 13:36:41 -0700

    swap interger

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  *swap_int - Swaps two intergers
+  *@a: first interger
+  *@b: second interger
+  *Return: void
+*/
+
+void swap_int(int *a, int *b)
+{
+	int *temp;
+
+	temp = *a;
+	*a = *b;
+	*b = *temp;
+}
