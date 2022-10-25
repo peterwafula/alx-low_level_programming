d7a15e12cb92684198a2da1f24c377d800a51592
tiptopy <tiptopywafula@gmail.com>
2022-10-25 13:46:38 -0700

    Prints string lngth

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+#include <stdio.h>
+
+/**
+  *_strlen - returns string length
+  *@*s: the pointer to a string.
+  *Return: Return lenght of a string
+*/
+
+int _strlen(char *s)
+{
+	return (strlen(*s));
+}
