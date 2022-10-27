006b5d4042aa4632a063c288565e7c81787d92b4
tiptopy <tiptopywafula@gmail.com>
2022-10-26 19:46:55 -0700

    half sstring

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  *puts_half - prints half string
+  *@str: string
+  *Return: void
+*/
+
+void puts_half(char *str)
+{
+	int i, j, k;
+
+	i = 0;
+
+	while (str[i] != '\0')
+		i++;
+	if (i % 2 == 0)
+		j = i / 2;
+	else
+		j = (i - 1) / 2;
+	for (k = j; k <= i; k++)
+		_putchar(str[k]);
+	_putchar('\n');
+}
