298839d834a8a296cdaf6591fe780071bd0b4810
tiptopy <tiptopywafula@gmail.com>
2022-10-26 19:25:46 -0700

    alternate string

---
+++ 0x05-pointers_arrays_strings/6-put2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  *puts2 - print string alternating
+  *@str: string to be printed
+  *Return: void
+*/
+
+void puts2(char *str);
+{
+	int i;
+
+	i = 0;
+	while(str[i] != '\0')
+		i++;
+	for (j = 0; j <= i; j += 2)
+		_putchar(str[j]);
+	_putchar('\n');
+}
