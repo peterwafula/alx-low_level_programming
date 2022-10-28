a8f3f1a220d97de7290719a35039eb11cfd4e427
tiptopy <tiptopywafula@gmail.com>
2022-10-28 10:44:56 -0700

    String comparisonL

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+#include <string.h>
+
+/**
+  *_strcmp - compares two strings
+  *@s1: first string to be compared
+  *@s2: Second string to be compared
+  *Return: comparison result as integer
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
