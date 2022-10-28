3b592ec7142dfa34b13216e6e2b27f3828b5b2a0
tiptopy <tiptopywafula@gmail.com>
2022-10-28 15:43:31 -0700

    sone with

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,14 @@
+#Finclude "main.h"
+
+#include <ctype.h>
+
+/**
+  *string_toupper - converts string to upper
+  *@s: string to be capitalized
+  *Return: ccapitals
+*/
+
+char *string_toupper(char *s);
+{
+	return (toupper(s));
+}
