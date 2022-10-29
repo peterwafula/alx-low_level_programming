3bf37c4e7bff6fb7539a5768d49ef51380885357
tiptopy <tiptopywafula@gmail.com>
2022-10-29 09:52:33 -0700

    updated typo

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,7 +1,5 @@
 #include "main.h"
 
-#include <ctype.h>
-
 /**
   *string_toupper - converts string to upper
   *@s: string to be capitalized
@@ -12,7 +10,7 @@ char *string_toupper(char *s)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0', i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] >= 'a' && s[i] <= 'z')
 			s[i] = s[i] - 32;
