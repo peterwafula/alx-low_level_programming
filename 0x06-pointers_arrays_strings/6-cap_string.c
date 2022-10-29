05ae951b678f4f7215680037f5a32c74ad63f9ab
tiptopy <tiptopywafula@gmail.com>
2022-10-29 12:38:18 -0700

    Capitalize word

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,31 +1,31 @@
 #include "main.h"
 
 /**
-  *cap_string - capitalizes all words of a string
-  *@s: input string.
-  *Return: the pointer to dest.
+  *cap_string - Capitalixe each word
+  *@s: string to be capitalized
+  *Return: Capitalized word
 */
 
 char *cap_string(char *s)
 {
-	int count = 0, i;
-	int separators[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+	int i;
+	char sep[13] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
 
-	if (*(s + count) >= 97 && *(s + count) <= 122)
-		*(s + count) = *(s + count) - 32;
-	count++;
-	while (*(s + count) != '\0')
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (i = 0; i < 13; i++)
+		if (i == 0)
 		{
-			if (*(s + count) == separators[i])
+			if (s[i] >= 'a' && s[i] <= 'z')
+				s[i] = s[i] - 32;
+		}
+		for (j = 0; j < 13; j++)
+		{
+			if (s[i] == sep[j])
 			{
-				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
-					*(s + (count + 1)) = *(s + (count + 1)) - 32;
-				break;
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+					s[i + 1] -= 32;
 			}
 		}
-		count+++;
 	}
 	return (s);
 }
