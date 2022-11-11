406ca4e411414c58417dab6762cf0f9a37c9b144
tiptopy <tiptopywafula@gmail.com>
2022-11-10 18:26:46 -0800

    fixed typo

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -6,6 +6,7 @@
   *create_array - this function creates an array
   *@size: size of memory
   *@c: character to be saved
+  *Return: pointer to the array
 */
 
 char *create_array(unsigned int size, char c)
@@ -13,15 +14,15 @@ char *create_array(unsigned int size, char c)
 	char *arr;
 	unsigned int i;
 
-	arr = (char *) malloc(sizeof(char) * size);
-	if(arr == NULL)
+	arr = (char *) malloc sizeof(char) * size);
+	if (arr == NULL)
 		return (0);
-	if(size == 0)
+	if (size == 0)
 		return (0);
 
 	for(i = 0; i <= size; i++)
 	{
-		if(i == size)
+		if (i == size)
 			arr[i] = '\0';
 		else
 			arr[i] = c;
