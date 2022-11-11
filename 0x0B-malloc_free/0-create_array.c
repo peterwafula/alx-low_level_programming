0bb566c6e7bd1dfa3f729e7f1fcb074480f09367
tiptopy <tiptopywafula@gmail.com>
2022-11-10 18:18:11 -0800

    Journey with memory allocation in ALX

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  *create_array - this function creates an array
+  *@size: size of memory
+  *@c: character to be saved
+*/
+
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+	int i;
+
+	arr = (char *) malloc(sizeof(char) * size);
+	if(arr == NULL)
+		return (0);
+	if(size == 0)
+		return (0);
+
+	for(i = 0; i <= size; i++)
+	{
+		if(i == size)
+			p[i] = '\0';
+		else
+			p[i] = c;
+	}
+	return (arr);
+}
